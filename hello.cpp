82396ea30427942f5cfbcd8522c9d192e55f31af
Loi <54327146+lhuynh7@users.noreply.github.com>
2019-09-03 15:15:32 -0600

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+
+{
+    cout<<"## Who you are?" <<endl;
+	cout<<"Hello, My name is Loi Huynh" <<endl;
+	cout<<"## Where are you from?" <<endl;
+    cout<<"I am from Albuquerque NM"<<endl;
+    cout<<"## Major " <<endl;
+    cout<<"My major at UNM is Computer Engineer"<<endl;
+    cout<<"## Favorite pastime " <<endl;
+    cout<<"I enjoy working on cars"<<endl;
+	cout<<"## Expection from class" <<endl;
+    cout<<"I am hoping to gain new skills and techinque to utilize C++"<<endl;
+    
+    return 0;
+}
---
+++ hello.md
@@ -0,0 +1,10 @@
+## Who you are?
+Hello, My name is Loi Huynh
+## Where are you from?
+I am from Albuquerque NM
+## Major 
+My major at UNM is Computer Engineer
+## Favorite pastime 
+I enjoy working on cars
+## Expection from class
+I am hoping to gain new skills and techinque to utilize C++
